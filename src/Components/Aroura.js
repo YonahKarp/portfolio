ec758501ada322fda79533b5cdc8aecc0b87beb3
Yonah Karp <yonah.karp@aexp.com>
2022-07-10 01:53:40 -0400

    updates

---
+++ src/Components/Aroura.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+
+// import '../scss/aroura.scss'
+
+export default class Aroura extends Component {
+  render() {
+    return (
+    <div class="bg">
+        <div class="auror_bg"></div>
+        <div class="aur_cont">
+          <span class="aur aur_1"></span>
+          <span class="aur aur_2"></span>
+          <span class="aur aur_3"></span>
+          <span class="aur aur_4"></span>
+          <span class="aur aur_5"></span>
+          <span class="aur aur_6"></span>
+          <span class="aur aur_7"></span>
+          <span class="aur aur_8"></span>
+          <span class="aur aur_9"></span>
+          <span class="aur aur_10"></span>
+          <span class="aur aur_11"></span>
+          <span class="aur aur_12"></span>
+          <span class="aur aur_13"></span>
+          <span class="aur aur_14"></span>
+          <span class="aur aur_15"></span>
+          <span class="aur aur_16"></span>
+          <span class="aur aur_17"></span>
+          <span class="aur aur_18"></span>
+          <span class="aur aur_19"></span>
+          <span class="aur aur_20"></span>
+          <span class="aur aur_21"></span>
+          <span class="aur aur_22"></span>
+          <span class="aur aur_23"></span>
+          <span class="aur aur_24"></span>
+        </div>
+      </div>
+    )
+  }
+}
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Aroura from './Aroura';
 
 class Header extends Component {
   render() {
@@ -25,17 +26,19 @@ class Header extends Component {
             <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
             <li><a className="smoothscroll" href="#about">About</a></li>
 	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
-            {/* <li><a className="smoothscroll" href="#portfolio">Works</a></li>
-            <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li> */}
+            {/* <li><a className="smoothscroll" href="#portfolio">Works</a></li> */}
+            <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
             <li><a className="smoothscroll" href="#contact">Schedule</a></li>
          </ul>
 
       </nav>
 
+      <Aroura/>
+
       <div className="row banner">
          <div className="banner-text">
             <h1 className="responsive-headline">I'm {name}.</h1>
-            <h3>I'm a {city} based <span>{occupation}</span>. {description}.</h3>
+            <h3>I'm a {city} based <span>{occupation}</span> with {description}.</h3>
             <hr />
             <ul className="social">
                {networks}
