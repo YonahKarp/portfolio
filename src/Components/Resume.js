7ba2555a2403f5e7c62e775fbf133a9c7b42ddd7
Yonah Karp <yonah.karp@aexp.com>
2022-07-11 11:37:15 -0400

    added some styling

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,27 +1,56 @@
-import React, { Component } from 'react';
+import React, { Component, useRef } from 'react';
 
-class Resume extends Component {
-  render() {
+import useIsVisible from '../Hooks/UseIsVisible';
 
-    if(this.props.data){
-      var skillmessage = this.props.data.skillmessage;
-      var education = this.props.data.education.map(function(education){
+
+const Resume = (props) =>{
+  const elemRef = useRef();
+  const isVisible = useIsVisible(elemRef);
+
+    if(props.data){
+      var skillmessage = props.data.skillmessage;
+      var education = props.data.education.map(function(education){
+        var eduImage = 'images/icons/'+education.image;
         return <div key={education.school}><h3>{education.school}</h3>
-        <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
+        <p className="info">
+          <img src={eduImage} className='logo'/>
+          {education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
         <p>{education.description}</p></div>
       })
-      var work = this.props.data.work.map(function(work){
+      var work = props.data.work.map(function(work){
+        var workImage = 'images/icons/'+work.image;
+
         return <div key={work.company}><h3>{work.company}</h3>
-            <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
+            <p className="info">
+            <img src={workImage} className='logo'/>
+              {work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
             <ul className='workItems'>{work.description.map((item, i) => <li key={i}>
               {item}
             </li>)}</ul>
         </div>
       })
-      var skills = this.props.data.skills.map(function(skills){
-        var className = 'bar-expand '+skills.name.toLowerCase();
-        return <li key={skills.name}><span style={{width:skills.level}}className={className}></span><em>{skills.name}</em></li>
-      })
+      // var skills = this.props.data.skills.map(function(skills){
+      //   var className = 'bar-expand '+skills.name.toLowerCase();
+      //   return <li key={skills.name}>
+      //     <span style={{width:100 - skills.level + '%'}}className={className}></span><em>{skills.name}</em>
+      //   </li>
+      // })
+
+        var skills = props.data.skills.map((skillTypes) => <div  className="bars" key={skillTypes.type}>
+          <h3>{skillTypes.type}</h3>
+          {skillTypes.list.map(function(skill){
+              var className = 'bar-expand '+skill.name.toLowerCase();
+              var icon = skill.icon ? 'images/icons/' + skill.icon : false
+              return <li key={skill.name}>
+                <span style={{width:100 - skill.level + '%', '--barWidth':100 - skill.level + '%'}} className={className}></span>
+                <em>
+                  {skill.name} 
+                  {icon? <img src={icon}/> : null} 
+                </em>
+              </li>
+            })}
+
+        </div>)
     }
 
     return (
@@ -55,27 +84,22 @@ class Resume extends Component {
 
 
 
-      {/* <div className="row skill">
+      <div className="row skill" ref={elemRef}>
 
          <div className="three columns header-col">
             <h1><span>Skills</span></h1>
          </div>
 
-         <div className="nine columns main-col">
-
-            <p>{skillmessage}
-            </p>
-
-				<div className="bars">
-				   <ul className="skills">
-					  {skills}
-					</ul>
-				</div>
-			</div>
-      </div> */}
+        <div className="nine columns main-col">
+          <p>{skillmessage}</p>
+        </div>
+        {isVisible && <div class='skillsHolder'>
+          {skills}
+        </div>}
+      </div>
    </section>
     );
   }
-}
+
 
 export default Resume;
