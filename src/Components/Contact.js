081a8433b5e23face0d71afdbd9303fef7df0411
Yonah Karp <yonah.karp@aexp.com>
2022-07-27 12:59:53 -0400

    webp images, timeline colors, calendar click message

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,9 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 
 import '../scss/Contact.scss'
 
 class Contact extends Component {
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      showMessage: false
+    }
+  }
+
+  o
+
+  handleIframeClick(){
+    this.setState({showMessage: true})
+  }
+
+  componentDidMount(){
+
+    let that = this
+    window.addEventListener('blur',function(){	
+      window.setTimeout(function () {	
+        if (document.activeElement instanceof HTMLIFrameElement) {
+          that.handleIframeClick()
+        }
+       }, 0);
+    }, {once : true});
+  }
+
   render() {
+
+
 
     if(this.props.data){
       var phone= this.props.data.phone;
@@ -41,12 +70,24 @@ class Contact extends Component {
 
          </div>
 
+         
+        <div className='iframeContainer'  onBlurCapture={this.state.showMessage ? null :(e) => this.handleIframeClick(e)}>
          <iframe title='Interview Availability'
+            className={(this.state.showMessage ? ' showMsg': '')}
+           
+           
             src="https://calendar.google.com/calendar/embed?src=lteb047e0dmvl741j1funhu674%40group.calendar.google.com&ctz=America%2FNew_York&mode=Week" 
             width="800" height="600" 
-            frameBorder="0" scrolling="no"></iframe>
+            frameBorder="0" scrolling="no" ></iframe>
+          <div className='hoverMsg'>
+            <h4>Note</h4>
+            <hr/>
+            <p>You can only <i>view</i> availability on this embeded calendar. </p>
+            <p>To schedule, please use Contact Details above  </p>
+          </div>
 
-   </section>
+        </div>
+      </section>
     );
   }
 }
--- src/Components/Timeline.js
+++ src/Components/Timeline.js
@@ -2,7 +2,30 @@ import React, {Fragment} from 'react';
 
 import '../scss/Timeline.scss'
 
-const start = new Date('January 2012')
+
+function safeDate(dateStr){
+    let dateMap = {
+        "January":  '01/01/',
+        "February": '02/01/',
+        "March":    '03/01/',
+        "April":    '04/01/',
+        "May":      '05/01/',
+        "June":     '06/01/',
+        "July":     '07/01/',
+        "August":   '08/01/',
+        "September":'09/01/',
+        "October":  '10/01/',
+        "November": '11/01/',
+        "December": '12/01/'
+    }
+
+    const [month, year] = dateStr.split(' ')
+    const safeStr = dateMap[month] + year
+
+    return new Date(safeStr)
+} 
+
+const start = safeDate('January 2012')
 const now = new Date()
 
 const n_years = (now.getFullYear() - start.getFullYear()) + 1
@@ -13,6 +36,9 @@ const years = Array.from({length: n_years}, (_, i) => now.getFullYear() - i )
 
 
 const Timeline = (props) =>{
+
+    const education = [...props.data.education]
+    
     const items = [...props.data.education, ...props.data.work].map(e => ({
         name: e.school || e.company,
         ...e
@@ -20,8 +46,8 @@ const Timeline = (props) =>{
 
     let sorted = items.sort((a,b)=> {
         
-        const a_end = a.end == 'Present' ? new Date() : new Date(a.end)
-        const b_end = b.end == 'Present' ? new Date() : new Date(b.end)
+        const a_end = a.end == 'Present' ? new Date() : safeDate(a.end)
+        const b_end = b.end == 'Present' ? new Date() : safeDate(b.end)
         
         return b_end - a_end
     })
@@ -56,12 +82,10 @@ const Timeline = (props) =>{
         </div>
         <div className='items'>
             {sorted.map((item) => {
-                const endDate = item.end == 'Present' ? new Date() : new Date(item.end)
-                const startDate = new Date(item.start)
+                const endDate = item.end == 'Present' ? new Date() : safeDate(item.end)
+                const startDate = safeDate(item.start)
                 const top = 100*(now - endDate) / (now - start)
                 const height = 100*(endDate - startDate) / (now - start)
-
-
 
                 return <a className='item smoothscroll_noHash'  
                     href={'#'+item.id}
@@ -77,7 +101,6 @@ const Timeline = (props) =>{
                         }),
                         ...(item.end == 'Present' && { borderTop: 'unset'})
                     }}>
-                        {/* {item.name} */}
                     </a>
             })}
         </div>
